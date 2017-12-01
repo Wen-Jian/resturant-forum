36926509e2cc34e145cb1e16433a93c773b167a5
Dex15900122 <e5900122@yahoo.com.tw>
2017-12-01 22:27:24 +0800

    set up admin authentication

--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -1,4 +1,4 @@
 class Admin::RestaurantsController < ApplicationController
-  def index
-  end
+  before_action :authenticate_user!
+  before_action :authenticate_admin
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+
+validates_presence_of :admin
+  def admin?
+      self.role == "admin"
+  end
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
@@ -9,5 +15,18 @@ class User < ApplicationRecord
   has_many :comments
   has_many :likes
   has_many :evaluations
+
+
+
+  validates_presence_of :name
+
+       def first_name
+         self.name.split.first
+       end
+       def last_name
+         self.name.split.last_name
+       end
+
+
 
 end
